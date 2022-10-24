3cbb0b063a7b8cee505960986f11445b791d6833
Soyka <petr.emelianov@icloud.com>
2022-10-24 10:52:20 +0300

    3

--- main.py
+++ main.py
@@ -62,3 +62,48 @@ def get_items_urls(file_path):
             file.write(f"{url}\n")
             
     return "[INFO] Urls collected successfully!"
+
+
+    def get_data(file_path):
+    with open(file_path) as file:
+        # urls_list = file.readlines()
+        
+        # clear_urls_list = []
+        # for url in urls_list:
+        #     url = url.strip()
+        #     clear_urls_list.append(url)
+        # print(clear_urls_list)
+        
+        urls_list = [url.strip() for url in file.readlines()]
+        
+    result_list = []
+    urls_count = len(urls_list)
+    count = 1
+    for url in urls_list:
+        response = requests.get(url=url, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml")
+        
+        try:
+            item_name = soup.find("span", {"itemprop": "name"}).text.strip()
+        except Exception as _ex:
+            item_name = None
+            
+        item_phones_list = []
+        try:
+            item_phones = soup.find("div", class_="service-phones-list").find_all("a", class_="js-phone-number")
+            
+            for phone in item_phones:
+                item_phone = phone.get("href").split(":")[-1].strip()
+                item_phones_list.append(item_phone)
+        except Exception as _ex:
+            item_phones_list = None
+            
+        try:
+            item_address = soup.find("address", class_="iblock").text.strip()
+        except Exception as _ex:
+            item_address = None
+            
+        try:
+            item_site = soup.find(text=re.compile("Сайт|Официальный сайт")).find_next().text.strip()
+        except Exception as _ex:
+            item_site = None
